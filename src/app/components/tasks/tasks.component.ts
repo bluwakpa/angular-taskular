111bd0138945971ed7c02256ef5b75fbd46c1c6f
bluwakpa <yezkablu@gmail.com>
2021-08-23 20:06:22 -0700

    debug init

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from './../data/task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks:Task[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.tasks = [
+      {
+        content: 'First Task',
+        completed: false
+      },
+      {
+        content: 'Second Task',
+        completed: false
+      }
+    ]
+  }
+
+  toggleDone(id:number){
+    this.tasks.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    })
+  }
+
+  deleteTask(id:number){
+    this.tasks = this.tasks.filter((v, i) => i !== id);
+  }
+}
