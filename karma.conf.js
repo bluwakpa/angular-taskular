a6422fb865559d80e3cf0241321c0ba4690f09fb
bluwakpa <yezkablu@gmail.com>
2021-10-31 21:11:42 -0700

    karma noscript

--- karma.conf.js
+++ karma.conf.js
@@ -42,3 +42,212 @@ module.exports = function (config) {
     restartOnFileChange: true
   });
 };
+
+{/* <span class="detailItem">
+  <span class="detailLabel">LAST UPDATED:
+  </span>
+  <span class="detailValue"> 2021-09-30 19:09:46 GMT
+  </span>
+  </span>
+  <div id="tagTestDetail">
+  </div>
+  </td>
+  </tr>
+  <tr>
+    <td>
+      <select id="tagTypeSelect" onchange=location.replace(this.value)>
+  <option  selected value="https://www.flashtalking.net/v3/tags/5715064.html">Javascript
+  </option>
+  <option  value="https://www.flashtalking.net/v3/tags/5715064_iframe.html">iFrame
+  </option>
+  <option  value="https://www.flashtalking.net/v3/tags/5715064_async.html">Async
+  </option>
+  </select>
+  </td>
+  </tr>
+  </table><script language="Javascript1.1" type="text/javascript" src="../geoSpoof/cookieCheck_23.js">
+  </script>
+    <div id="Working" style="width:300px;height:250px;margin: 0 auto;text-align:center;">
+      <script language="Javascript1.1" type="text/javascript">
+        if(window.location.search.indexOf("loc=")>0){
+          var geoSpoof = "&MII_IP=" + window.location.search.substr(window.location.search.indexOf("loc=")+4);
+        }
+          else{var geoSpoof = "";
+        }
+          if(window.location.search.indexOf("time=")>0){
+            geoSpoof += "&virtualDateTime=" + window.location.search.substr(window.location.search.indexOf("time=")+5, 15);
+        }
+      </script>
+        <noscript><a href="https://servedby.flashtalking.com/click/2/144025;5715064;0;209;0/?gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_78}&ft_width=300&ft_height=250&url=29737035" target="_blank">
+        <img border="0" src="https://servedby.flashtalking.com/imp/2/144025;5715064;205;gif;ChristianScienceMonitor;PagefoldTestingBackupIssueStandardAd/?'+(typeof geoSpoof === 'undefined'?'':geoSpoof)+'&gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_78}">
+      </a>
+        </noscript>
+        <script language="Javascript1.1" type="text/javascript">
+          var ftClick = "";var ftExpTrack_5715064 = "";var ftX = "";var ftY = "";var ftZ = "";var ftOBA = 1;var ftContent = "";var ftCustom = "";var ft300x250_OOBclickTrack = "";var ftRandom = Math.random()*1000000;var ftBuildTag1 = "<scr";var ftBuildTag2 = "</";var ftClick_5715064 = ftClick;if(typeof(ft_referrer)=="undefined"){
+            var ft_referrer=(function(){var r="";if(window==top){
+              r=window.location.href;
+              }
+              else{
+                try{
+                  r=window.parent.location.href;
+                  }
+                  catch(e){
+                  }
+                  r=(r)?r:document.referrer;
+                  }
+                  while(encodeURIComponent(r).length>1000){
+                  r=r.substring(0,r.length-1);
+                  }
+                  return r;
+                  }
+                  ());
+                  }var ftDomain = (window==top)?"":(function(){
+                  var d=document.referrer,h=(d)?d.match("(?::q/q/)+([qw-]+(q.[qw-]+)+)(q/)?".replace(/q/g,decodeURIComponent("%"+"5C")))[1]:"";
+                  return (h&&h!=location.host)?"&ft_ifb=1&ft_domain="+encodeURIComponent(h):"";
+                  }
+                  ());
+                  var ftCQs="&gdpr=${GDPR}&gdpr_consent=${
+                  GDPR_CONSENT_78}";
+                  var ftTag = ftBuildTag1 + 'ipt language="javascript1.1" type="text/javascript" ';
+                  ftTag += 'src="https://servedby.flashtalking.com/imp/2/144025;
+                  5715064;201;js;ChristianScienceMonitor;PagefoldTestingBackupIssueStandardAd/?'+
+                  (typeof geoSpoof === 'undefined'?'':geoSpoof)+
+                  '&ftx='+ftX+'&fty='+ftY+'&ftadz='+ftZ+'&ftscw='+ftContent+'&ft_custom='+ftCustom+'&ftOBA='+ftOBA+ftDomain+
+                  '&ft_agentEnv='+(window.mraid||window.ormma?'1':'0')+
+                  '&ftiniframe=false'+geoSpoof+'&ft_referrer='+encodeURIComponent(ft_referrer)+ftCQs+
+                  '&cachebuster='+ftRandom+'" id="ftscript_300x250" name="ftscript_300x250"';
+                  ftTag += '>' + ftBuildTag2 + 'script>';document.write(ftTag);
+        </script>
+        </div>
+        <TEXTAREA onclick="this.focus();this.select()" ID="holdtext" STYLE="display:none;">
+        </TEXTAREA>
+        <div id="xcopy" style="margin: 0 auto;font-family:	Arial, Helvetica, sans-serif;color:  #606060;font-size:  10pt; text-align:center;">
+          <br>
+            <br>
+              <table width="700px" border="0" align="center" cellpadding="0px" cellspacing="0px">
+                <tr>
+                  <td colspan="2">
+                    <strong>Notes to publisher: 
+                    </strong>
+                  </td>
+                </tr>
+                  <tr>
+                    <td>1)
+                    </td>
+                      <td>Flashtalking tags are auto cachebusting and do not require a random number or timestamp. 
+                      </td>
+                  </tr>
+                  <tr>
+                    <td>2)
+                    </td>
+                      <td>If you wish to add your own click counting code (eg %c for DART) enter the code in the Third Party Click box and click update tag. 
+                      </td>
+                    <tr>
+                      <td>3)
+                      </td>
+                    <td>Escape options (select option and click update tag):
+                    </td>
+                    </tr>
+              <tr>
+                <td>
+                  <input type="radio" name="escape" value=0 checked>
+                </td>
+                <td>No escaping required
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="radio" name="escape" value=1></td><td>Escape click variable, this is usually only necessary for adservers who use ampersand (&) characters in their click counting code.</td></tr><tr><td><input type="radio" name="escape" value=2></td><td>Double escape click variable, this is usually only necessary for particular sites (e.g Guardian.co.uk)</td></tr><tr><td colspan="2">
+                    <div align="center">
+                      <label>Third Party Click:
+                      </label>
+                      <input type="textbox" id="clickvar" value="" style="width:200px">
+                    </div>
+                    <tr>
+                      <td>4)
+                    </td>
+                    <td>Enter values in the boxes below to set custom x,y, z-index and content width settings.  For help 
+                      <a style="color: #606060;" href="../../helpSystem/tags/customPositioning.pdf" target="_blank">click here
+                      </a>.
+                    </td>
+                    </tr>
+                    <tr>
+                      <td colspan="2">
+                        <div align="center">
+                          x:
+                          <input type="textbox" id="ftx" value="" style="width:50px">  
+                            y:
+                            <input type="textbox" id="fty" value="" style="width:50px">  
+                              z:
+                              <input type="textbox" id="ftz" value="" style="width:50px">  
+                                content width:
+                                <input type="textbox" id="ftcontent" value="" style="width:50px">  
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        5)
+                      </td>
+                      <td>Add Network Conversion Tracking:
+                        <input type="checkbox" name="netconv" id="netconv" onclick="if(document.getElementById('netconv').checked){display('show','netconvin')}else{display('hide','netconvin')}" unchecked>&nbsp;&nbsp;
+                          <input type="textbox" id="netconvin" value="" style="width:200;display:none"> For help 
+                            <a style="color: #606060;" href="../../helpSystem/tags/customConversionTracking.pdf" target="_blank">
+                              click here
+                            </a>
+                              .
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        6)
+                      </td>
+                      <td>Disable OBA Notification (if active on tag):
+                        <input type="checkbox" name="obaOption" id="oba_opt" unchecked>&nbsp;For help 
+                          <a style="color: #606060;" href="../../helpSystem/tags/disableOBA.pdf" target="_blank">click here
+                          </a>.
+                      </td>
+                      </tr>
+                      <tr>
+                      <td>7)
+                      </td>
+                      <td>Add Expand tracking:
+                        <input type="checkbox" name="expandTrack" id="expandTrack" onclick="if(document.getElementById('expandTrack').checked){display('show','expandTrackin')}else{display('hide','expandTrackin')}" unchecked>&nbsp;&nbsp;<input type="textbox" id="expandTrackin" value="" style="width:200px;display:none">For help 
+                          <a style="color: #606060;" href="../../helpSystem/tags/expandTracking.pdf" target="_blank">click here
+                          </a>.
+                      </td>
+                      </tr>
+                      <tr>
+                        <td>8)
+                        </td>
+                        <td>Add mraid code:
+                          <input type="checkbox" name=mraidOption" id="mraid_opt" unchecked>&nbsp;For help 
+                            <a style="color: #606060;" href="../../helpSystem/tags/mraid.pdf" target="_blank">click here
+                            </a>.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colspane="2">&nbsp;
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colspan="2">
+                          <div align="center">
+                            <input type="button" onClick="amend()" id="amend" value="Update Tag" style="height:23px">
+                          </div>
+                        </td>
+                      </tr>
+                        <td colspan="2">
+                          <div align="center" id="copydiv" style="display:none">
+                            <input type="button" onClick="ClipBoard()" id="copy" value="Copy tag to clipboard" style="height:23px">
+                          </div>
+                            <script language="javascript">if (document.all){var xcopy = document.all.copydiv.style;xcopy.display="block";}
+                            </script>
+                        </td>
+                      </tr>
+                  </table>
+                    <br>
+                </div>
+                  <div id="xtag" style="margin: 0px auto;">
+                    <textarea onclick="this.focus();this.select()" style="width:1000px;height:350px" id="showtag">
+                      <noscript> */}
